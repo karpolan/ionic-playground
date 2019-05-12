acd0ca91e5ab01d5208d5f228706c277ce7066c0
Anton Karpenko <karpolan@karpolan.com>
2019-05-12 09:15:34 +0300

    Tests updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -49,9 +49,11 @@ describe('AppComponent', () => {
     await fixture.detectChanges();
     const app = fixture.nativeElement;
     const menuItems = app.querySelectorAll('ion-label');
-    expect(menuItems.length).toEqual(2);
+    expect(menuItems.length).toEqual(4);
     expect(menuItems[0].textContent).toContain('Home');
-    expect(menuItems[1].textContent).toContain('List');
+    expect(menuItems[1].textContent).toContain('Settings');
+    expect(menuItems[2].textContent).toContain('Contact');
+    expect(menuItems[3].textContent).toContain('About');
   });
 
   it('should have urls', async () => {
@@ -59,9 +61,11 @@ describe('AppComponent', () => {
     await fixture.detectChanges();
     const app = fixture.nativeElement;
     const menuItems = app.querySelectorAll('ion-item');
-    expect(menuItems.length).toEqual(2);
+    expect(menuItems.length).toEqual(4);
     expect(menuItems[0].getAttribute('ng-reflect-router-link')).toEqual('/home');
-    expect(menuItems[1].getAttribute('ng-reflect-router-link')).toEqual('/list');
+    expect(menuItems[1].getAttribute('ng-reflect-router-link')).toEqual('/settings');
+    expect(menuItems[2].getAttribute('ng-reflect-router-link')).toEqual('/contact');
+    expect(menuItems[3].getAttribute('ng-reflect-router-link')).toEqual('/about');
   });
 
 });
